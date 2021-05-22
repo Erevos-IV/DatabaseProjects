c3770e0faf351809b354675872468a4a070e0219
Vasileios Goysetis <83865356+Erevos-IV@users.noreply.github.com>
2021-05-22 16:24:49 +0300

    Update StudentExams.sql

--- CollegeDatabase/StoredProcedures/StudentExams.sql
+++ CollegeDatabase/StoredProcedures/StudentExams.sql
@@ -1,12 +1,12 @@
 -- Using College Database
 USE college;
 
--- Getting the Exams of a given student
+-- This procedure will give us the exams details of a given student.
 DELIMITER //
 CREATE PROCEDURE StudentExams (IN StuName VARCHAR(50))
 	BEGIN
     
-	SELECT stu.StudentID, CONCAT(stu.FirstName, ' ', stu.LastName) AS Name, stu.Phone, ex.ExamDate, exd.Grade
+    SELECT stu.StudentID, CONCAT(stu.FirstName, ' ', stu.LastName) AS Name, stu.Phone, ex.ExamDate, exd.Grade
     FROM students AS stu 
     INNER JOIN exams AS ex USING(ProgramID)
     INNER JOIN examdetails as exd USING(StudentID)
