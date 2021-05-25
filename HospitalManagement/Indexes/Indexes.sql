42430b6418ea4f79f4999f3507b490034fa9cf3f
Vasileios Goysetis <83865356+Erevos-IV@users.noreply.github.com>
2021-05-25 09:42:51 +0300

    Add files via upload

---
+++ HospitalManagement/Indexes/Indexes.sql
@@ -0,0 +1,18 @@
+-- Using Hospital database.
+USE hospital;
+
+-- Creating index in AMKA (which is a unique series of numbers owning by people after 16 years old)
+CREATE UNIQUE INDEX Patients_AMKA_idx
+ON patients(AMKA);
+
+-- Creating index in patient's phone so that searching will be faster than before.
+CREATE UNIQUE INDEX Patients_Phone_idx
+ON patients(Phone);
+
+-- Creating index in employee's  Last Name.
+CREATE UNIQUE INDEX Employee_LastName_idx
+ON employees(LastName);
+
+-- Creating index in Departments.
+CREATE UNIQUE INDEX Departments_ID_idx
+ON departments(DepartmentID);
