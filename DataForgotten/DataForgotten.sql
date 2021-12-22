274b3a43ebb95f991e6b4f40bb510014c2290773
Vasileios Goysetis <83865356+Erevos-IV@users.noreply.github.com>
2021-12-22 17:28:06 +0200

    Update DataForgotten.sql

--- DataForgotten/DataForgotten.sql
+++ DataForgotten/DataForgotten.sql
@@ -1,1 +1,12 @@
-j
+-- MSSQL
+CREATE PROCEDURE Users.SP_DeleteAccountAndData
+	@username nvarchar(50),
+	@password nvarchar(50)
+
+AS
+	SET NOCOUNT ON;
+
+	DELETE 
+	FROM Users
+	WHERE Username = @username AND Password = @password;
+GO
